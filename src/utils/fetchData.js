a0b31e20deacd35db60df58faf150ac96951566b
rohan209547mourya <rohan.209547@gmail.com>
2023-02-03 23:11:32 +0530

    api key updated

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -7,8 +7,8 @@ const options = {
     maxResults: 50,
   },
   headers: {
-    "X-RapidAPI-Key": "12a0cf228emsha08113ec4981a4bp1c5bb4jsn3257c9bad288",
-    // "X-RapidAPI-Key": "f45682ebdcmsh03cb86adc0e8f8cp11d3dfjsn46832adf6556",
+    // "X-RapidAPI-Key": "12a0cf228emsha08113ec4981a4bp1c5bb4jsn3257c9bad288",
+    "X-RapidAPI-Key": "f45682ebdcmsh03cb86adc0e8f8cp11d3dfjsn46832adf6556",
     "X-RapidAPI-Host": "youtube-v31.p.rapidapi.com",
   },
 };
